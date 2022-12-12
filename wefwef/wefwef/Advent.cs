fed12c2761c460e79c6e0a412c6a25ad1df1ce4f
Ropke13 <79408886+Ropke13@users.noreply.github.com>
2022-12-12 22:49:47 +0200

    day12

--- wefwef/wefwef/Advent.cs
+++ wefwef/wefwef/Advent.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Numerics;
@@ -13,9 +14,9 @@ namespace wefwef
         public class Directory
         {
             public string Dir { get; set; }
-            public int Size { get; set; }
+            public long Size { get; set; }
 
-            public Directory(string dir, int size)
+            public Directory(string dir, long size)
             {
                 Dir = dir;
                 Size = size;
@@ -436,7 +437,102 @@ namespace wefwef
         }
         public static void Day12()
         {
+            var inputFile = File.ReadAllLines("input12.txt");
+            var input = new List<string>(inputFile);
 
+            var start = (x: -1, y: -1);
+            var end = (x: -1, y: -1);
+
+            var map = new List<List<char>>();
+
+            var distanceCosts = new Dictionary<(int, int), int>();
+
+            foreach (var line in input)
+            {
+                var charList = new List<char>();
+                foreach (char c in line)
+                {
+                    if (c == 'S')
+                    {
+                        start = (charList.Count, map.Count);
+                        charList.Add('a');
+                    }
+                    else if (c == 'E')
+                    {
+                        end = (charList.Count, map.Count);
+                        distanceCosts[end] = 0;
+                        charList.Add('z');
+                    }
+                    else charList.Add(c);
+
+                }
+                map.Add(charList);
+            }
+
+            explorePaths(end.x, end.y);
+
+            void explorePaths(int x, int y)
+            {
+                int currentCost = distanceCosts[(x, y)];
+
+                neighbourCost(x + 1, y);
+                neighbourCost(x - 1, y);
+                neighbourCost(x, y + 1);
+                neighbourCost(x, y - 1);
+
+                void neighbourCost(int newX, int newY)
+                {
+                    if (newX < 0 || newX >= map[0].Count) return;
+                    if (newY < 0 || newY >= map.Count) return;
+
+
+                    if (map[newY][newX] + 1 >= map[y][x])
+                    {
+                        if (!distanceCosts.ContainsKey((newX, newY)) || distanceCosts[(newX, newY)] > currentCost + 1)
+                        {
+                            distanceCosts[(newX, newY)] = currentCost + 1;
+                            explorePaths(newX, newY);
+                        }
+                    }
+                }
+            }
+
+            void Part1()
+            {
+                Console.WriteLine(distanceCosts[(start.x, start.y)]);
+            }
+
+            void Part2()
+            {
+                var startinA = new List<(int, int)>();
+
+                for (int row = 0; row < map.Count; row++)
+                {
+                    for (int coll = 0; coll < map[row].Count; coll++)
+                    {
+                        if (map[row][coll] == 'a')
+                        {
+                            startinA.Add((coll, row));
+                        }
+                    }
+                }
+
+                int lowest = Int32.MaxValue;
+
+                foreach (var coord in startinA)
+                {
+                    if (!distanceCosts.ContainsKey(coord)) continue;
+
+                    var count = distanceCosts[coord];
+                    if (count < lowest) lowest = count;
+                }
+                Console.WriteLine(lowest);
+            }
+
+            //Part1();
+            //Part2();
         }
+        
+
     }
 }
