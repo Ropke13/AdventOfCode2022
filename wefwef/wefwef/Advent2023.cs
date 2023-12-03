846833ab291eae127b03ea333cdaf402a8ab7f98
Ropke13 <79408886+Ropke13@users.noreply.github.com>
2023-12-03 22:13:59 +0200

    day 3 doen

--- wefwef/wefwef/Advent2023.cs
+++ wefwef/wefwef/Advent2023.cs
@@ -88,15 +88,15 @@ namespace wefwef
             int lowestRed = int.MinValue;
             int lowestGreen = int.MinValue;
 
-            foreach (var line in input) 
+            foreach (var line in input)
             {
                 string[] removeGame = line.Split(':');
                 string[] gameSets = removeGame[1].Split(';');
 
-                for(int i = 0; i < gameSets.Length; i++)
+                for (int i = 0; i < gameSets.Length; i++)
                 {
                     string[] games = gameSets[i].Split(',');
-                    for(int j = 0; j < games.Length; j++) 
+                    for (int j = 0; j < games.Length; j++)
                     {
                         string[] numberColor = games[j].Split(' ');
 
@@ -105,7 +105,7 @@ namespace wefwef
                             if (int.Parse(numberColor[1]) > lowestBlue)
                             {
                                 lowestBlue = int.Parse(numberColor[1]);
-                            } 
+                            }
                         }
                         else if (numberColor[2] == "red")
                         {
@@ -132,6 +132,143 @@ namespace wefwef
             }
 
             Console.WriteLine(sum.ToString());
+        }
+
+        public class NumberNode
+        {
+            public string Number { get; set; }
+            public Tuple<int, int> Gear { get; set; }
+            public NumberNode(string number, Tuple<int, int> gear)
+            {
+                Number = number;
+                Gear = gear;
+            }
+        }
+
+        public static void Day3()
+        {
+            var input = File.ReadAllLines("input3-2023.txt");
+            string currentNumber = "";
+            int lineNumber = 0;
+            bool addNumber = true;
+            int sum = 0;
+            List<NumberNode> numbers = new List<NumberNode>();
+            List<Tuple<int, int>> currentNumberCoords = new List<Tuple<int, int>>();
+            Tuple<int, int> forGear = null;
+
+            foreach (var line in input)
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (Char.IsNumber(line[i]))
+                    {
+                        currentNumber += line[i];
+
+                        if (lineNumber != 0)
+                        {
+                            if (input[lineNumber - 1][i] == '*' && !Char.IsNumber(input[lineNumber - 1][i]))
+                            {
+                                addNumber = false;
+                                forGear = Tuple.Create(lineNumber - 1, i);
+                            }
+
+                            if (i != line.Length - 1)
+                            {
+                                if (input[lineNumber - 1][i + 1] == '*' && !Char.IsNumber(input[lineNumber - 1][i + 1]))
+                                {
+                                    addNumber = false;
+                                    forGear = Tuple.Create(lineNumber - 1, i + 1);
+                                }
+                            }
+                        }
+
+                        if (lineNumber != input.Length - 1)
+                        {
+                            if (input[lineNumber + 1][i] == '*' && !Char.IsNumber(input[lineNumber + 1][i]))
+                            {
+                                addNumber = false;
+                                forGear = Tuple.Create(lineNumber + 1, i);
+                            }
+
+                            if (i != line.Length - 1 && i != 0)
+                            {
+                                if (input[lineNumber + 1][i - 1] == '*' && !Char.IsNumber(input[lineNumber + 1][i - 1]))
+                                {
+                                    addNumber = false;
+                                    forGear = Tuple.Create(lineNumber+1, i-1);
+                                }
+                            }
+                        }
+
+                        if (i != 0)
+                        {
+                            if (line[i - 1] == '*' && !Char.IsNumber(line[i - 1]))
+                            {
+                                addNumber = false;
+                                forGear = Tuple.Create(lineNumber, i - 1);
+                            }
+
+                            if (lineNumber != 0)
+                            {
+                                if (input[lineNumber - 1][i - 1] == '*' && !Char.IsNumber(input[lineNumber - 1][i - 1]))
+                                {
+                                    addNumber = false;
+                                    forGear = Tuple.Create(lineNumber-1, i - 1);
+                                }
+                            }
+                        }
+
+
+                        if (i != input.Length - 1)
+                        {
+                            if (line[i + 1] == '*' && !Char.IsNumber(line[i + 1]))
+                            {
+                                addNumber = false;
+                                forGear = Tuple.Create(lineNumber, i+1);
+                            }
+
+                            if (lineNumber != input.Length - 1)
+                            {
+                                if (input[lineNumber + 1][i + 1] == '*' && !Char.IsNumber(input[lineNumber + 1][i + 1]))
+                                {
+                                    addNumber = false;
+                                    forGear = Tuple.Create(lineNumber + 1, i + 1);
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (currentNumber != string.Empty && !addNumber)
+                        {
+                            NumberNode item = new NumberNode(currentNumber, forGear);
+                            numbers.Add(item);
+
+                            currentNumber = string.Empty;
+                            addNumber = true;
+                        }
+                        else
+                        {
+                            currentNumber = string.Empty;
+                            addNumber = true;
+                        }
+                    }
+                }
+                lineNumber++;
+            }
+
+            var sortedNumbers = numbers.OrderBy(f => f.Gear).ToList();
+
+            for (int i = 0; i < sortedNumbers.Count-1; i++)
+            { 
+                if(sortedNumbers[i].Gear.ToString() == sortedNumbers[i + 1].Gear.ToString())
+                {
+                    sum += int.Parse(sortedNumbers[i].Number) * int.Parse(sortedNumbers[i + 1].Number);
+                    //Console.WriteLine("{0}-{1}     {2}-{3}    {4}", sortedNumbers[i].Number, sortedNumbers[i].Gear.ToString(), sortedNumbers[i + 1].Number, sortedNumbers[i + 1].Gear.ToString(), sum);
+                }
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
